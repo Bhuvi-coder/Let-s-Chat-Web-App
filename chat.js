caa99c3f2b19b023978218d05745038a96fb9f08
Bhuvi-coder <147069292+Bhuvi-coder@users.noreply.github.com>
2024-03-05 18:30:14 +0530

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,19 +1,17 @@
 // Your web app's Firebase configuration
 //ADD YOUR FIREBASE LINKS HERE
 const firebaseConfig = {
-    apiKey: "AIzaSyCkLGfjS3NfG3ep_Kl74aICX_zDfcdxYmk",
-    authDomain: "let-s-chat-web-app-ee87e.firebaseapp.com",
-    databaseURL: "https://let-s-chat-web-app-ee87e-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "let-s-chat-web-app-ee87e",
-    storageBucket: "let-s-chat-web-app-ee87e.appspot.com",
-    messagingSenderId: "814404124278",
-    appId: "1:814404124278:web:0159b955768a852d8a6957"
+    apiKey: "AIzaSyCDKQpFcEktC-sFoTbbYUYI89-2tCeqvA4",
+    authDomain: "projects-3c3d9.firebaseapp.com",
+    databaseURL: "https://projects-3c3d9-default-rtdb.firebaseio.com",
+    projectId: "projects-3c3d9",
+    storageBucket: "projects-3c3d9.appspot.com",
+    messagingSenderId: "304559370555",
+    appId: "1:304559370555:web:13b8df6efedf1fab17071e"
   };
   
+    firebase.initializeApp(firebaseConfig);
   
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 
 function addUser()
